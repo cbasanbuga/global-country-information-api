11de1d841946bfe80bf45ac6a8c3d3d077432acf
Cumali Batuhan AŞANBUĞA <asanbuga@gmail.com>
2023-08-26 00:11:49 +0300

    Intial Commit

---
+++ src/main/java/com/cbasanbuga/globalcountryinformationapi/GlobalCountryInformationApiApplication.java
@@ -0,0 +1,13 @@
+package com.cbasanbuga.globalcountryinformationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalCountryInformationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalCountryInformationApiApplication.class, args);
+	}
+
+}
